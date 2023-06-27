2d6e81b0b0d5de6ea649d8e8e6f42d1905155862
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-06-27 17:22:07 +0900

    Update add picture function in MemoFragment

--- app/app/src/main/java/com/example/happybankbook/MemoFragment.java
+++ app/app/src/main/java/com/example/happybankbook/MemoFragment.java
@@ -1,64 +1,88 @@
 package com.example.happybankbook;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MemoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MemoFragment extends Fragment {
+import com.bumptech.glide.Glide;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class MemoFragment extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView txtAddPicture;
+    private TextView txtSave;
+    private ImageView img;
 
-    public MemoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MemoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MemoFragment newInstance(String param1, String param2) {
-        MemoFragment fragment = new MemoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getGallery();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_memo, container, false);
+        View view=inflater.inflate(R.layout.fragment_memo, container, false);
+        init(view);
+        return view;
     }
+
+    public void init(View view){
+        txtAddPicture=view.findViewById(R.id.addPicture);
+        txtSave=view.findViewById(R.id.save);
+        img=view.findViewById(R.id.imageView);
+
+        txtAddPicture.setOnClickListener(this);
+        txtSave.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+       if(v.getId()==R.id.addPicture){
+            loadImage();
+       }else if(v.getId()==R.id.save){
+            save();
+       }
+    }
+
+    public void getGallery(){
+
+        img.setVisibility(View.VISIBLE);
+
+        activityResultLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),result -> {
+            if(result.getResultCode()==RESULT_OK&&result!=null&&result.getData()!=null){
+                Uri imageUri=result.getData().getData();
+                Glide.with(getContext()).load(imageUri).into(img);
+            }
+        });
+
+    }
+
+    public void loadImage(){
+        Intent intent=new Intent(Intent.ACTION_GET_CONTENT).
+                setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,"image/*");
+        Intent createChooserIntent=Intent.createChooser(intent,null);
+        activityResultLauncher.launch(createChooserIntent);
+    }
+
+
+    public void save(){
+
+    }
+
 }
