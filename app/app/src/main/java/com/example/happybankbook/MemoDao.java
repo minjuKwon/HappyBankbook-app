4676448dbec1878046e5f1053899187d3532a429
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-07-04 17:24:00 +0900

    Update MemoDao.java
    -insertMemo()
    -searchKeyword()
    -searchDuration()
    -searchCount()
    -sortDateAsc()
    -sortDateDesc()

--- app/app/src/main/java/com/example/happybankbook/MemoDao.java
+++ app/app/src/main/java/com/example/happybankbook/MemoDao.java
@@ -2,10 +2,29 @@ package com.example.happybankbook;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface MemoDao {
 
-    void add(MemoData memo);
+    @Insert
+    void insertMemo(MemoData memo);
+
+    @Query("SELECT * FROM MemoData_table WHERE content LIKE '%' || :keyword || '%'")
+    List<MemoData> searchKeyword(String keyword);
+
+    @Query("SELECT * FROM MemoData_table WHERE date BETWEEN :from AND :to")
+    List<MemoData> searchDuration(int from, int to);
+
+    @Query("SELECT * FROM MemoData_table LIMIT :cnt")
+    List<MemoData> searchCount(int cnt);
+
+    @Query("SELECT * FROM MemoData_table ORDER BY date ASC")
+    List<MemoData> sortDateAsc();
+
+    @Query("SELECT * FROM MemoData_table ORDER BY date DESC")
+    List<MemoData> sortDateDesc();
 
 }
