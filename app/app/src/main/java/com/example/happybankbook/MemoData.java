8df960ff627a71986df64400a3ae3ea43ea60376
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-07-19 17:48:23 +0900

    Update getter int MemoData

--- app/app/src/main/java/com/example/happybankbook/MemoData.java
+++ app/app/src/main/java/com/example/happybankbook/MemoData.java
@@ -27,4 +27,24 @@ public class MemoData {
     public void setContent(String content){this.content=content;}
     public void setBitmap(Bitmap image){this.image=image;}
 
+    public int getIdx() {
+        return idx;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+    
 }
