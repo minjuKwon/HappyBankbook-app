5a442a11cb561f5499262f1accdd2ac99b59d620
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-07-27 16:51:57 +0900

    Update view and presenter to extend in ListContract

--- app/app/src/main/java/com/example/happybankbook/contract/ListContract.java
+++ app/app/src/main/java/com/example/happybankbook/contract/ListContract.java
@@ -3,17 +3,18 @@ package com.example.happybankbook.contract;
 import com.example.happybankbook.db.MemoDao;
 import com.example.happybankbook.db.MemoData;
 import com.example.happybankbook.presenter.BasePresenter;
+import com.example.happybankbook.presenter.DataPresenter;
+import com.example.happybankbook.view.BaseView;
 
 import java.util.ArrayList;
 
 public interface ListContract {
 
-    interface View{
-        void setItems(ArrayList<MemoData> items);
+    interface View extends BaseView {
+
     }
 
-    interface Presenter extends BasePresenter {
-        void setView(ListContract.View view);
+    interface Presenter extends DataPresenter<View> {
         void getData(MemoDao memoDao);
     }
 
