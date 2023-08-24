e41b4312567d51c9f261d2a706dd9554c3fa45dc
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-08-24 17:03:26 +0900

    Create BaseItemView.java

---
+++ app/app/src/main/java/com/example/happybankbook/adapter/BaseItemView.java
@@ -0,0 +1,12 @@
+package com.example.happybankbook.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BaseItemView extends RecyclerView.ViewHolder {
+    public BaseItemView(@NonNull View view){
+        super(view);
+    }
+}
