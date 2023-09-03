6f1c2fdfc20da0b74b4aabd02e01b10a494396fb
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-09-03 22:24:54 +0900

    Add view id in setting fragment

--- app/app/src/main/java/com/example/happybankbook/view/SettingFragment.java
+++ app/app/src/main/java/com/example/happybankbook/view/SettingFragment.java
@@ -7,60 +7,40 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.example.happybankbook.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SettingFragment extends Fragment {
+public class SettingFragment extends Fragment{
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SettingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingFragment newInstance(String param1, String param2) {
-        SettingFragment fragment = new SettingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private TextView txtManual, txtEllipsis, txtPdf, txtExcel, txtTxt, txtOpenSource;
+    private RadioGroup radioGroupLine, radioGroupFont;
+    private RadioButton radioSingle, radioMull, radioOne, radioTwo, radioThree;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+       View view=inflater.inflate(R.layout.fragment_setting, container, false);
+       init(view);
+        return view;
     }
+
+    private void init(View view){
+        txtManual=view.findViewById(R.id.manual);
+        txtEllipsis=view.findViewById(R.id.ellipsis);
+        txtPdf=view.findViewById(R.id.pdf);
+        txtExcel=view.findViewById(R.id.excel);
+        txtTxt=view.findViewById(R.id.txt);
+        txtOpenSource=view.findViewById(R.id.openSource);
+        radioGroupLine=view.findViewById(R.id.radioLineDisplay);
+        radioSingle=view.findViewById(R.id.radioLineSingle);
+        radioMull=view.findViewById(R.id.radioLineMul);
+        radioGroupFont=view.findViewById(R.id.radioFont);
+        radioOne=view.findViewById(R.id.radioFontOne);
+        radioTwo=view.findViewById(R.id.radioFontTwo);
+        radioThree=view.findViewById(R.id.radioFontThree);
+    }
+
 }
