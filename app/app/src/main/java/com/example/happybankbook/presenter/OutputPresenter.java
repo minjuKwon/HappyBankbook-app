cb164318e810d56ae67916a01a2cdb68ab67618d
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-09-07 20:24:53 +0900

    Create OutputPresenter.java

---
+++ app/app/src/main/java/com/example/happybankbook/presenter/OutputPresenter.java
@@ -0,0 +1,57 @@
+package com.example.happybankbook.presenter;
+
+import android.util.Log;
+
+import com.example.happybankbook.GetReturnValue;
+import com.example.happybankbook.contract.OutputContract;
+import com.example.happybankbook.db.MemoDao;
+import com.example.happybankbook.db.MemoData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class OutputPresenter implements OutputContract.Presenter {
+
+    private CompositeDisposable disposable;
+
+    GetReturnValue getReturnValue;
+
+    public OutputPresenter(){
+        this.disposable=new CompositeDisposable();
+    }
+
+    @Override
+    public void releaseView() {
+        disposable.clear();
+    }
+
+    @Override
+    public void getDataToFile(MemoDao memoDao, char split) {
+        StringBuffer stringBuffer=new StringBuffer();
+        disposable.add(
+                memoDao.getAll()
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(
+                                item->{
+                                    for(MemoData data : (ArrayList<MemoData>)item){
+                                        stringBuffer.append(data.getNum()).append(split)
+                                                .append(data.getDate()).append(split)
+                                                .append(data.getContent()).append(split)
+                                                .append(data.getPrice()).append('\n');
+                                    }
+                                    getReturnValue.getStringBuffer(stringBuffer);
+                                },
+                                error->{Log.d("Memo","export file data error : "+error);}
+                        )
+        );
+    }
+
+    public void setGetReturnValue(GetReturnValue getReturnValue){
+        this.getReturnValue=getReturnValue;
+    }
+
+}
