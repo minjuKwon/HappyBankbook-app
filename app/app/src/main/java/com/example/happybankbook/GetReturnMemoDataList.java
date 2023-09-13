19321e08c3bf7f02c0a3f216c4295a1054a10529
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-09-13 16:14:32 +0900

    Create GetReturnMemoDataList.java

---
+++ app/app/src/main/java/com/example/happybankbook/GetReturnMemoDataList.java
@@ -0,0 +1,9 @@
+package com.example.happybankbook;
+
+import com.example.happybankbook.db.MemoData;
+
+import java.util.ArrayList;
+
+public interface GetReturnMemoDataList {
+    void getMemoDataList(ArrayList<MemoData>list);
+}
