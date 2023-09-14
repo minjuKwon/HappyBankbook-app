d8d91aecb1873972e312802797166a8a88db8a46
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-09-14 16:22:50 +0900

    Create PdfRunnable for convert db to pdf

---
+++ app/app/src/main/java/com/example/happybankbook/PdfRunnable.java
@@ -0,0 +1,128 @@
+package com.example.happybankbook;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.graphics.pdf.PdfDocument;
+import android.os.Environment;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.Log;
+
+import androidx.core.content.ContextCompat;
+
+import com.example.happybankbook.db.MemoData;
+import com.example.happybankbook.view.SettingFragment;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class PdfRunnable implements Runnable{
+
+    private ArrayList<MemoData> dataList;
+    private Context context;
+    private int width=1080;
+    private int height=1920;
+
+    public PdfRunnable(ArrayList<MemoData> dataList, Context context){
+        this.dataList=dataList;
+        this.context=context;
+    }
+
+    @Override
+    public void run() {
+
+        PdfDocument pdfDocument=new PdfDocument();
+        PdfDocument.PageInfo pageInfo;
+        PdfDocument.Page page;
+        Canvas canvas;
+
+        for(int i=0;i<dataList.size();i++){
+
+            pageInfo=new PdfDocument.PageInfo.Builder(width, height, 1).create();
+            page=pdfDocument.startPage(pageInfo);
+
+            //canvas 배경 색 지정
+            canvas=page.getCanvas();
+            canvas.drawColor(ContextCompat.getColor(context,R.color.cream));
+
+            //canvas 배경 drawable 지정
+            int dy=height/15;
+            Drawable backgroundColor=ContextCompat.getDrawable(context,R.drawable.memo_writing);
+            backgroundColor.setBounds(0, dy, width, height-dy);
+            backgroundColor.draw(canvas);
+
+            //date
+            int date=dataList.get(i).getDate();
+            int year=date/10000;
+            date-=year*10000;
+            int month=date/100;
+            int day=date%100;
+
+            TextPaint paintDate=new TextPaint();
+            paintDate.setTextSize(64);
+            paintDate.setTextAlign(Paint.Align.CENTER);
+            paintDate.setUnderlineText(true);
+            canvas.drawText(String.format("%d.%02d.%02d",year,month,day),width/2, dy+180, paintDate);
+
+            //image
+            if(dataList.get(i).getImage()!=null){
+                Bitmap img=resizeBitmap(dataList.get(i).getImage(), 560, 420);
+                Paint paintImg=new Paint();
+                paintImg.setAntiAlias(true);
+                canvas.drawBitmap(img,(width-img.getWidth())/2,dy+240, paintImg);
+
+            }
+
+            //content
+            String content=dataList.get(i).getContent();
+            TextPaint paintContent=new TextPaint();
+            paintContent.setTextSize(48);
+            paintContent.setTextAlign(Paint.Align.CENTER);
+            StaticLayout.Builder builder=StaticLayout.Builder.obtain(
+                    content, 0, content.length(), paintContent, width-400);
+            StaticLayout staticLayout=builder.build();
+            canvas.save();
+            canvas.translate(width/2, 928);
+            staticLayout.draw(canvas);
+            canvas.restore();
+
+            //price
+            int price=dataList.get(i).getPrice();
+            TextPaint paintPrice=new TextPaint();
+            paintPrice.setTextSize(54);
+            paintPrice.setTypeface(Typeface.create(Typeface.DEFAULT,Typeface.BOLD));
+            paintPrice.setTextAlign(Paint.Align.CENTER);
+            canvas.drawText(String.valueOf(price),width/2, height-dy+87, paintPrice);
+
+            //clover
+            Drawable clover=ContextCompat.getDrawable(context,R.drawable.clover_30);
+            clover.setBounds(width/2-220, height-dy+40, width/2-160, height-dy+100);
+            clover.draw(canvas);
+
+            pdfDocument.finishPage(page);
+
+        }
+
+        pdfDocument.close();
+
+    }
+
+    public Bitmap resizeBitmap(Bitmap bitmap, int newWidth, int newHeight){
+        int width=bitmap.getWidth();
+        int height=bitmap.getHeight();
+        while(width>=newWidth||height>=newHeight){
+            width*=0.9;
+            height*=0.9;
+        }
+        return Bitmap.createScaledBitmap(bitmap, width, height, true);
+    }
+
+}
