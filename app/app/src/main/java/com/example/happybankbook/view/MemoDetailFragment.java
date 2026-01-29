37e9263665e066d5a2863a2fbd0bdc76707c8734
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2026-01-29 20:43:47 +0900

    Fix: Update last position check condition

--- app/app/src/main/java/com/example/happybankbook/view/MemoDetailFragment.java
+++ app/app/src/main/java/com/example/happybankbook/view/MemoDetailFragment.java
@@ -70,7 +70,7 @@ public class MemoDetailFragment extends Fragment implements ListContract.View,Vi
     };
 
     private float textSize= TEXT_SIZE_DEFAULT_SMALL;
-    private int itemCount, fromDate, toDate, rowCount, currentPosition;
+    private int itemCount, fromDate, toDate, currentPosition;
     private boolean isNewestSort;
     private Integer pendingItemId;
 
@@ -128,8 +128,6 @@ public class MemoDetailFragment extends Fragment implements ListContract.View,Vi
         itemCount= state.count;
         isNewestSort= state.isNewestSort;
 
-        getRowCount();
-
         changePage();
     }
 
@@ -140,12 +138,8 @@ public class MemoDetailFragment extends Fragment implements ListContract.View,Vi
         }
     }
 
-    private void getRowCount(){
-        presenter.setIntResultCallback(value -> rowCount=value);
-        presenter.getDataCount();
-    }
-
     private void changePage(){
+        ListConditionState state = ((MainActivity) requireActivity()).getListState();
         viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
@@ -154,7 +148,7 @@ public class MemoDetailFragment extends Fragment implements ListContract.View,Vi
                 if(currentPosition==0){
                     forwardImageView.setVisibility(View.INVISIBLE);
                     backImageView.setVisibility(View.VISIBLE);
-                }else if(currentPosition==(rowCount-1)){
+                }else if(currentPosition==(state.count-1)){
                     forwardImageView.setVisibility(View.VISIBLE);
                     backImageView.setVisibility(View.INVISIBLE);
                 }else{
